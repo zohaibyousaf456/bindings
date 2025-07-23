42820a735e22d6bfca9852b38a64ead23fc16d9b
Zohaibaidevgen <aidevgenzohaib@gmail.com>
2025-07-23 18:55:44 +0500

    chore: add assetPrefix for jsDelivr@v1.0.1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  assetPrefix: 'https://cdn.jsdelivr.net/gh/zohaibyousaf456/my-static-next@v1.0.1',
 };
 
 export default nextConfig;
